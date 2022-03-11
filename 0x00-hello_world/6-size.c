5cf3bc7180d11c0c93667d1f5d05eeeee6c4c6ae
OKA2019 <kounapetri05@gmail.com>
2022-03-11 01:27:45 +0000

    Write a script that generates the assembly code (Intel syntax) of a C code and save it in an output file.

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char a;
+	int a;
+	long int a;
+	long long int a;
+	float a;
+	return (0);
+}
