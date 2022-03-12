e31a60e34125141390bd79189192f30af01b9ed9
OKA2019 <kounapetri05@gmail.com>
2022-03-12 18:43:25 +0000

    10. update

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,17 +1,23 @@
 #include <stdio.h>
 
-
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	char a;
-	int a;
-	long int a;
-	long long int a;
-	float a;
+	int b;
+	long int c;
+	long long int d;
+	float e;
+
+	sizeof(a);
+	sizeof(b);
+	sizeof(c);
+	sizeof(d);
+	sizeof(e);
 	return (0);
 }
