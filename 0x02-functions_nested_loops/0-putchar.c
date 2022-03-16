84bbc1d9a9ec752e040a7418db17c90f4164940b
OKA2019 <kounapetri05@gmail.com>
2022-03-16 23:48:35 +0000

    0. _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include "main.h"
+
 /**
  * main - chek the code
  *
@@ -6,6 +8,12 @@
  */
 int main(void)
 {
-	printf("_putchar");
+	char putchar[8]="_putchar";
+	int i;
+	
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(putchar[i]);
+	}
 	return (0);
 }
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,4 @@
+#ifndef H_PUTCHAR_H
+#define H_PUTCHAR_H
+	int _putchar(char c);
+#endif
