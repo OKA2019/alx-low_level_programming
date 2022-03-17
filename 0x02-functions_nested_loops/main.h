21e9fcefeb2ea0f391f3f913094381d6932231b9
OKA2019 <kounapetri05@gmail.com>
2022-03-17 01:03:18 +0000

    2. 10 x alphabet

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -3,7 +3,15 @@
 	int _putchar(char c);
 #endif
 
+
 #ifndef H_PRINT_ALPHABET_H
 #define H_PRINT_ALPHABET_H
 	void print_alphabet();
+#endif
+
+
+
+#ifndef H_PRINT_ALPHABET_x10_H
+#define H_PRINT_ALPHABET_x10_H
+	void print_alphabet_x10();
 #endif
