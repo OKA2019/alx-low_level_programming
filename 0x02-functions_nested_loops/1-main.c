498c47369d507a51e697ca2bdb6efc22481371f0
OKA2019 <kounapetri05@gmail.com>
2022-03-17 00:40:15 +0000

    1. I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

---
+++ 0x02-functions_nested_loops/1-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - chek the code
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
+
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,3 +2,8 @@
 #define H_PUTCHAR_H
 	int _putchar(char c);
 #endif
+
+#ifndef H_PRINT_ALPHABET_H
+#define H_PRINT_ALPHABET_H
+	void print_alphabet();
+#endif
