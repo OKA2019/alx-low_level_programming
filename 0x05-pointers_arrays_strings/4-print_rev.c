dc4e70e96f507eae00dc6e808b7e1edc1c75cd09
OKA2019 <kounapetri05@gmail.com>
2022-03-22 02:37:11 +0000

    4. I can only go one way. I've not got a reverse gear

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _puts -  prints a string, in reverse, followed by a new line.
+ * print_rev -  prints a string, in reverse, followed by a new line.
  * @s: pointer
  * Return: Always 0;
  */
