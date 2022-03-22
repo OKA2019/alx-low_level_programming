af1a4ac3180b7727a77a9c74b69786152cf010a7
OKA2019 <kounapetri05@gmail.com>
2022-03-22 21:33:43 +0000

    9. strcpy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,12 +1,12 @@
 #include <string.h>
 /**
- * *_strcpy - function that copies the string pointed to by src, including the terminating
+ * *_strcpy - function that copies the string pointed to by src
  * @dest: pointer
  * @src: pointer
  * Return: Pointer to `dest`
  */
 char *_strcpy(char *dest, char *src)
 {
-	strcpy(dest,src);
+	strcpy(dest, src);
 	return (dest);
 }
