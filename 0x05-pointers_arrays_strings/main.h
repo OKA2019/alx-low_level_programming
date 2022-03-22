542d6db5424236f0d40be07511c038739c9d18dc
OKA2019 <kounapetri05@gmail.com>
2022-03-22 01:57:51 +0000

    3. I do not fear computers. I fear the lack of them

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,3 +1,8 @@
+#ifndef H_PUTCHAR_H
+#define H_PUTCHAR_H
+	int _putchar(char c);
+#endif
+
 #ifndef RESET_TO_98_H
 #define RESET_TO_98_H
 	void reset_to_98(int *n);
@@ -7,4 +12,10 @@
 #define SWAP_INT_H
         void swap_int(int *a, int *b);
 #endif /*SWAP_INT_H*/
+
+#ifndef _PUTS_H
+#define _PUTS_H
+        void _puts(char *str);
+#endif /*_PUTS_H*/
+
 
