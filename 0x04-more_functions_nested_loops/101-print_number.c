8ded67b4a26019c11174b24904448476ea0686dc
OKA2019 <kounapetri05@gmail.com>
2022-03-27 17:51:50 +0000

    9. Fizz-Buzz

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer.
+ * @n: integer
+ * Return: Always O
+ */
+void print_number(int n)
+{
+	int i;
+	char c;
+
+	while (i = 0; i < 10; i++)
+	{
+		c = m;
+		if (c > 9)
+		{
+			_putchar('1');
+			c = m % 10;
+		}
+		_putchar('0' + c);
+	}
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -62,5 +62,10 @@
 
 
 
+#ifndef PRINT_NUMBER_H
+#define PRINT_NUMBER_H
+	void print_number(int n);
+#endif /*PRINT_NUMBER_H*/
+
 
 
