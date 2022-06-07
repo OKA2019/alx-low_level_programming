7af8ef576a5bf84b5af74c7cd2b33afd303b473a
oka2019 <kounapetri05@gmail.com>
2022-06-07 02:15:15 +0000

    11. The success combination in business is: Do what you do better... and: do more of what you do...

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -11,18 +11,23 @@ int main(void)
 {
 	int i, j, m;
 
-
+	i = '0';
+	j = '0';
+	m = '0';
 	for (i = '0'; i <= '8'; i++)
 	{
 		for (j = i + 1; j <= '9'; j++)
 		{
-                	for (m = j + 1; m <= '9'; m++)
-                	{
+			for (m = j + 1; m <= '9'; m++)
+			{
 				putchar(i);
 				putchar(j);
 				putchar(m);
-				putchar(',');
-				putchar(' ');
+				if (i != '7')
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
 		}
 	}
