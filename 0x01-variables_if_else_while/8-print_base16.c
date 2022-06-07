2f7d68453640c6de9d3c06251e4d1463791d15e6
oka2019 <kounapetri05@gmail.com>
2022-06-07 00:13:39 +0000

    8. Hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -6,21 +6,21 @@
  * Return: Always 0 (Success)
  */
 
-
 int main(void)
 {
-        char i;
-        char j;
+	char i;
+	char j;
 
-        for (i = 0; i <= '9'; i++)
-        {
-                putchar(i);
-        }
-        for (j = 'a'; j <= 'f'; j++)
-        {
-                putchar(j);
-        }
+	putchar(' ');
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar(i);
+	}
+	for (j = 'a'; j <= 'f'; j++)
+	{
+		putchar(j);
+	}
 	putchar('\n');
-        return (0);
+	return (0);
 }
 
